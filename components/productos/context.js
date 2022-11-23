df3c228ff37474e9ed34918c3c5704966fda4ffc
ayerein <ayelenreinglestein@gmail.com>
2022-11-22 22:24:09 -0300

    Correccion

--- components/productos/context.js
+++ components/productos/context.js
@@ -15,12 +15,23 @@ class Contenedor{
                 console.log(`Producto agregado! Id del producto: ${obj.id}`)
                 return await fs.promises.writeFile(this.archivo, JSON.stringify(productos, null, 2))
             } else {
-                let agregarProductos = JSON.parse(traerProductos)
-                obj.id = agregarProductos.length + 1
+                let agregarProductos = traerProductos
+                obj.id = agregarProductos[agregarProductos.length - 1].id + 1
                 agregarProductos.push(obj)
                 console.log(`Producto agregado! Id del producto: ${obj.id}`)
                 return await fs.promises.writeFile(this.archivo, JSON.stringify(agregarProductos, null, 2))
             }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async update(productoNuevo){
+        let traerProductos = await this.getAll()
+        let nuevoArray = traerProductos.filter(obj => obj.id != productoNuevo.id)
+        try{
+            nuevoArray.push(productoNuevo)
+            return await fs.promises.writeFile(this.archivo, JSON.stringify(nuevoArray, null, 2))
         } catch (error) {
             console.log(error)
         }
@@ -43,7 +54,7 @@ class Contenedor{
     async getAll(){
         try {
             let productosTodos = await fs.promises.readFile(this.archivo, 'utf-8')
-            return productosTodos
+            return JSON.parse(productosTodos)
         } catch (error) {
             console.log(error)
         }
@@ -52,28 +63,10 @@ class Contenedor{
     async deleteById(id){
         let traerProductos = await this.getAll()
         try {
-            let nuevoArray = JSON.parse(traerProductos).filter(obj => obj.id != id)
+            let nuevoArray = traerProductos.filter(obj => obj.id != id)
             return await fs.promises.writeFile(this.archivo, JSON.stringify(nuevoArray, null, 2))
         } catch (error) {
             console.log(error)
-        }
-    }
-
-    async updateById(obj){
-        let traerProductos = await this.getAll()
-        let productos = JSON.parse(traerProductos)
-        console.log(productos)
-        let index = productos.map(producto => producto.id).indexOf(Number(obj.id))
-        if (index >= 0){
-            try {
-                let nuevoArray = await this.deleteById(index +1)
-                let nuevoAgregar = await JSON.parse(nuevoArray).push(obj)
-                console.log(nuevoArray)
-                console.log(`Producto actualizado! Id del producto: ${obj.id}`)
-                return await fs.promises.writeFile(this.archivo, JSON.stringify(nuevoAgregar, null, 2))   
-            } catch (error) {
-                console.log(error)
-            }
         }
     }
 }
--- components/productos/index.js
+++ components/productos/index.js
@@ -1,23 +1,12 @@
 let {Router} = require("express")
 let router = new Router()
 
-/* const Container = require("../../context/context")
-const Productos = new Container('./productos.txt') */
-
 let Contenedor = require ('./context.js')
 
 const productos = new Contenedor('productos.txt')
 
-
-/* let agregarId = (req, res, next) => {
-    req.body.id = prods.length +1    
-    next()
-} */
-
-
 module.exports = app => {
     
-
     /* Traer todos los productos. */
     app.use("/api", router)
     router.get("/productos", async(req, res, next) => {
@@ -25,18 +14,14 @@ module.exports = app => {
         res.json(mostrarProductos)
     })
 
-
     /* Buscar producto por id. */
-    app.use("/api", router)
     router.get("/productos/:id", async(req, res, next) => {
         let id = Number(req.params.id)
         let productoId = await productos.getById(id)
         res.json(productoId)
     })
 
-
     /* Agregar un nuevo producto. */
-    app.use("/api", router)
     router.post("/productos", async(req, res, next) => {
         let productoNuevo = req.body
 
@@ -48,25 +33,23 @@ module.exports = app => {
         }
     })
 
-
     /* Actualizar un producto. */
-    app.use("/api", router)
-    router.put("/productos", async(req, res, next) => {
-        let productoNuevo = await req.body
-        await productos.updateById(productoNuevo)
-        res.send('Producto Actualizado') 
-
-        /* if (productoNuevo.id ) {
-            await productos.save(productoNuevo)
-            res.json({
-                newProduct: productoNuevo
-            })
-        } */
+    router.put("/productos/:id", async(req, res, next) => {
+        let traerProductos = await productos.getAll()
+        let id = Number(req.params.id)
+        let index = traerProductos.map(producto => producto.id).indexOf(id)
+        let productoNuevo = req.body
+        productoNuevo.id = id
+        if (index >= 0){
+            await productos.update(productoNuevo)
+            res.send('Producto actualizado!')
+        } else {
+            res.send('Id de producto no encontrado.')
+        }
     })
 
 
     /* Eliminar un producto por su id. */
-    app.use("/api", router)
     router.delete("/productos/:id", async(req, res, next) => {
         let mostrarProductos = await productos.getAll()
         let id = Number(req.params.id)
